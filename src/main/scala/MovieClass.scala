0e09711adcde8d3ea85c2c358200b4f6ad73542d
Elva Eglite <eglite.elva@gmail.com>
2020-09-28 22:46:14 +0300

    Enabled User Input

--- src/main/scala/MovieClass.scala
+++ src/main/scala/MovieClass.scala
@@ -1,5 +1,16 @@
 import sun.misc.FloatingDecimal
 
+/**
+ *
+ * @param title
+ * @param genre
+ * @param lead_studio
+ * @param audience_score
+ * @param profitability
+ * @param rotten_tomatoes_score
+ * @param worldwide_gross
+ * @param year
+ */
 final case class MovieClass(title: String,
                             genre:String,
                             lead_studio:String,
--- src/main/scala/Movie_playlists.scala
+++ src/main/scala/Movie_playlists.scala
@@ -1,12 +1,11 @@
 import java.io.FileWriter
+import scala.io.StdIn.readLine
 
 object Movie_playlists extends App{
 
 // Getting the text file from the User Directory
-  val userDir = System.getProperty("user.dir") // I used UserDirectory, so that the code is more universal
-  val srcName = s"$userDir/movieDB.txt" // TODO please check if this works for you, if ok, please delete the commented lines in this section
-//  val srcName = "c:/temp/movieDB.txt"
-//  val dstName = "c:/temp/DesiredPlaylist.txt"
+  val userDir = System.getProperty("user.dir") // UserDirectory makes the code more universal
+  val srcName = s"$userDir/movieDB.txt"
   val dstName = s"$userDir/DesiredPlaylist.txt"
 
   def openSource(fName:String) = {
@@ -15,9 +14,7 @@ object Movie_playlists extends App{
     myLines
   }
 
-//FIXME to get rid of duplicates (Gnomeo and Juliet)
-  //FIXME to get rid of "$" sign and change worldwide gross type to Double
-  //FIXME to get rid of showing "MovieClass" before the results
+//TODO to get rid of duplicate (Gnomeo and Juliet)
 
   //Counts lines in the file
   def getLineCount(fileName: String): Int = {
@@ -59,61 +56,61 @@ object Movie_playlists extends App{
     splitLineSeq.map(t => MovieClass(t.head,t(1),t(2),t(3).toInt,t(4),t(5).toInt,t(6),t(7).toInt))
   }
 
-  //Printing results in IntellJ - Data validation
+  //Printing results in IntellJ - for Data validation and User Input
   val lineCount = getLineCount(srcName)
-  println(s"We got $lineCount lines in our file.")
+  println(s"We got $lineCount movies in our list.")
   val myLineSplits = getLineSplits(srcName)
   val dataValid = if (myLineSplits.min == myLineSplits.max)
-    println("There are no missing data in lines. It is ok to proceed!")
+    println(s"We checked that there are no missing data in our movie list. " +
+      s"It is ok to proceed with your movie choice!\n")
     else println("Some lines might be missing some data. Please check!")
 
+  //Code for the User's Input
+  val userName = readLine("What is your name? ")
+  println(s"$userName, what kind of movie genre do you prefer:")
+  var userGenre = readLine ("(R)omance, (C)omedy, (D)rama, (A)nimation? or (Ac)tion? ")
 
   //Filters to Playlists
-  def getTop5byAudScore: Seq[MovieClass] = { //FIXME how to get rid of the text "MovieClass" before the results?
+  def getTop5byAudScore: Seq[MovieClass] = { //FIXME or TODO to create prettyPrint to get rid of the text "MovieClass" before the results?
     val rawSplit = getParsedLines(srcName)
     val filteredResults = rawSplit.filter(_.size == 8) // gets lines with split size 8
-    val ourMovies = getMovieClassSeq(filteredResults.slice(1, filteredResults.size)) // connects MovieClass with each split element
-    val sortByAudScore = ourMovies.sortBy(_.audience_score).reverse.slice(0, 5)
+    val ourMovies = getMovieClassSeq(filteredResults.slice(1, filteredResults.size)) // maps MovieClass with each split element
+    val sortByAudScore = ourMovies.sortBy(_.audience_score).reverse.filter(_.genre.startsWith(userGenre)).slice(0, 5)
     sortByAudScore
   }
 
-  def getTop5RotTom: Seq[MovieClass] = { //FIXME how to get rid of the text "MovieClass" before the results?
+  def getTop5RotTom: Seq[MovieClass] = {
     val rawSplit = getParsedLines(srcName)
     val filteredResults = rawSplit.filter(_.size == 8) // gets lines with split size 8
     val ourMovies = getMovieClassSeq(filteredResults.slice(1, filteredResults.size)) // connects MovieClass with each split element
-    val sortByTomScore = ourMovies.sortBy(_.rotten_tomatoes_score).reverse.slice(0, 5)
+    val sortByTomScore = ourMovies.sortBy(_.rotten_tomatoes_score).reverse.filter(_.genre.startsWith(userGenre)).slice(0, 5)
     sortByTomScore
   }
 
-  def getTop5RotTomComedy: Seq[MovieClass] = {
-    val rawSplit = getParsedLines(srcName)
-    val filteredResults = rawSplit.filter(_.size == 8) // gets lines with split size 8
-    val ourMovies = getMovieClassSeq(filteredResults.slice(1, filteredResults.size)) // connects MovieClass with each split element
-    val sortByTomScoreComedy = ourMovies.sortBy(_.rotten_tomatoes_score).reverse.filter(_.genre=="Comedy")slice (0, 5)
-    sortByTomScoreComedy
-  }
-
-  val top5Audience = "TOP 5 MOVIES BY AUDIENCE: \r\n"
+  val top5Audience = "TOP 5 MOVIES BY AUDIENCE SCORE: \r\n"
   val top5Tomatoes = "TOP 5 MOVIES FROM ROTTEN TOMATOES \r\n"
-  val top5TomComedies = "TOP 5 COMEDIES FROM ROTTEN TOMATOES \r\n"
-
 
   // Saving yielded data
-  def saveSeq(destName:String, mySeq:Seq[MovieClass], mySeq2:Seq[MovieClass],mySeq3:Seq[MovieClass]) = {
-    println(s"Saving my Sequence to file $destName")
+  def saveSeq(destName:String, mySeq:Seq[MovieClass], mySeq2:Seq[MovieClass]) = {
+    if (userGenre == "Ac") {
+      println(s"Sorry we have only one action movie. Its details are saved: $destName")
+    } else
+      println(s"Nice choice! Your TOP5 movie list is saved: $destName")
     val fw = new FileWriter(destName)
     fw.write(s"$top5Audience")//gets new section/playlist
     mySeq.map(_ + "\r\n").foreach(fw.write)
     fw.write(s"\r\n $top5Tomatoes")
     mySeq2.map(_ + "\r\n").foreach(fw.write)
-    fw.write(s"\r\n $top5TomComedies")
-    mySeq3.map(_ + "\r\n").foreach(fw.write)
     fw.close()
   }
 
   val mySeq = openSource(srcName)
-  saveSeq(dstName,getTop5byAudScore,getTop5RotTom,getTop5RotTomComedy)
-  //FIXME How to print only the titles and the criteria? Otherwise the results look kinda ugly
+  saveSeq(dstName,getTop5byAudScore,getTop5RotTom)
+
+  //TODO how to get rid of the text "MovieClass" before the results?
+  //TODO How to print only Title, Genre, Studio, Both Scores and Year? Otherwise the results look kinda ugly.
+  //This actually might be already integrated in the MovieClass PrettyPrint
+
 }
 //TODO create db and save the results there
 
